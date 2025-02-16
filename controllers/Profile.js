0cf2ad71560ebb15de9d62e89f1a65af51dfcd0f
Varunesh Pathak <varuneshpathak21@gmail.com>
2025-02-16 13:37:27 +0530

    Completed Profile

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -1,6 +1,9 @@
 import sendResponse from "../utlis/sendResponse.js";
 import User from "../models/User.js";
 import Profile from "../models/Profile.js";
+import { uploadImageOnCloudinary } from "../utlis/imageUploader.js";
+import { convertSecondsToDuration } from "../utlis/secToDuration.js";
+import CourseProgress from "../models/CourseProgess.js";
 
 const updateProfile = async(req, res) => {
     try {
@@ -55,8 +58,122 @@ const getAllUserDetails = async(req, res) => {
         return sendResponse(res, 500, false, "Unable to fetched all user");
     }
 }
+const updateDisplayPicture = async(req, res) => {
+    try {
+        const displayPic = req.files.displayPicture;
+        const userId = req.user.id;
+        const image = await uploadImageOnCloudinary(
+                            displayPic,
+                            process.env.FOLDER_NAME,
+                            1000,
+                            1000
+                        )
+        console.log(image);
+        const updateProfile = await User.findByIdAndUpdate(
+            {_id: userId},
+            { image: image.secure_url },
+            { new: true }
+        )
+        res.send({
+            success: true,
+            message: `Image Updated successfully`,
+            data: updateProfile
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+const getEnrolledCourses = async(req, res) => {
+    try {
+        const userId = req.user.id;
+        let userDetails = await User.findOne({
+                                _id: userId,
+                            })
+                            .populate({
+                                path: "courses",
+                                populate: {
+                                    path: "courseContent",
+                                    populate: {
+                                        path: "subSection"
+                                    }
+                                }
+                            })
+                            .exec();
+        userDetails = userDetails.toObject();
+        var SubsectionLength = 0;
+        for(var i=0; i < userDetails.courses.length; j++){
+            let totalDurationInSeconds = 0;
+            SubsectionLength = 0;
+            for(var j=0; j < userDetails.courses[i].courseContent.length; j++){
+                totalDurationInSeconds += userDetails.courses[i]
+                                            .courseContent[j]
+                                            .SubsectionLength
+                                            .reduce((acc, curr) => acc + parseInt(curr.timeDuration), 0)
+                
+                userDetails.courses[i].totalDuration = convertSecondsToDuration(totalDurationInSeconds)
+                Subsection += userDetails.courses[i].courseContent[j].subSection.length
+            } 
+            let courseProgressCount = await CourseProgress.findOne({
+                courseID: userDetails.courses[i]._id,
+                userId: userId
+            })
+            courseProgressCount = courseProgressCount?.completedVideos.length
+            if(SubsectionLength === 0){
+                userDetails.courses[i].progressPercentage = 100
+            } else {
+                const multiplier = Math.pow(10, 2)
+                userDetails.courses[i].progressPercentage = Math.round((courseProgressCount / SubsectionLength) * 100 * multiplier) / multiplier
+            }
+        }
+        if(!userDetails){
+            return res.status(400).json({
+                success: false,
+                message: `Could not find user with id: ${userDetails}`
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            data: userDetails.courses
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+const instructorDashboard = async(req, res) => {
+    try {
+        const courseDetails = await CourseProgress.find({instructor: req.user.id})
+
+        const courseData = courseDetails.map((course) => {
+            const totalStudentEnrolled = course.studentsEnrolled.length
+            const totalAmountGenerated = totalStudentEnrolled * course.price
+
+            const courseDataWithStats = {
+                _id: course._id,
+                courseName: course.courseName,
+                courseDescription: course.courseDescription,
+                totalStudentEnrolled,
+                totalAmountGenerated
+            }
+            return courseDataWithStats
+        })
+        res.status(200).json({
+            courses: courseData
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: " Internal Server Error" })
+    }
+}
 export {
     updateProfile,
     deleteProfile,
-    getAllUserDetails
+    getAllUserDetails,
+    updateDisplayPicture,
+    getEnrolledCourses
 }
